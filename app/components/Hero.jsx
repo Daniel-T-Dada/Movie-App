bc290793d99da498e8668532241767058bcab50c
Double D <simplytobs@gmail.com>
2024-09-20 14:23:25 +0100

    update

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -48,8 +48,7 @@ const Hero = () => {
                                     alt={slide.replace(/_/g, ' ').replace('.jpg', '')}
                                     width={800}
                                     height={900}
-                                    layout="responsive"
-                                    objectFit="cover"
+                                    style={{ objectFit: 'cover' }}
                                 />
                             </SwiperSlide>
                         ))}
@@ -58,7 +57,7 @@ const Hero = () => {
                             <svg viewBox="0 0 48 48" ref={progressCircle}>
                                 <circle cx="24" cy="24" r="20"></circle>
                             </svg>
-                        
+
                         </div>
                     </Swiper>
                 </div>
